cb329cd22215043bdffb148e233e5527d7f6a9d8
ST10452897 <162623508+ST10452897@users.noreply.github.com>
2026-05-03 21:40:54 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body>
+            <h1> Welcome to Pastimes <h1>
+                <p> This is our second-hand clothing store. </p>
+
+                <a href="login.php">Login</a><br>
+                <a href= "register.php">Register</a>
+</body>
+</html>

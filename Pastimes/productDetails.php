79524a93fb17f1ed60bb713bd31f5013c276863b
zinhlematshiane <ST10447353@rcconnect.edu.za>
2026-05-03 17:28:02 +0200

    added product and productDetails files

---
+++ Pastimes/productDetails.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Details</title>
+</head>
+<body>
+
+<h1>Vintage Shirt</h1>
+
+<img src="images/shirt1.jpg" width="300">
+
+<p>Price: R250</p>
+<p>Condition: Good</p>
+<p>Seller: John Doe</p>
+
+<button>Buy Now</button>
+
+</body>
+</html>
---
+++ Pastimes/products.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products</title>
+</head>
+<body>
+
+<h1>Available Clothing</h1>
+
+<div>
+    <img src="images/shirt1.jpg" width="200">
+    <h3>Vintage Shirt</h3>
+    <p>Price: R250</p>
+</div>
+
+<div>
+    <img src="images/hoodie1.jpg" width="200">
+    <h3>Black Hoodie</h3>
+    <p>Price: R350</p>
+</div>
+
+</body>
+</html>

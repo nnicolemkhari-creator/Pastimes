e9f5da41db87f86d38e68daa79bbdc1cea186605
kgantsho168 <129422741+kgantsho168@users.noreply.github.com>
2026-05-04 21:34:22 +0200

    Update Admin.php

--- Pastimes/Admin.php
+++ Pastimes/Admin.php
@@ -11,7 +11,7 @@ $users = $conn->query("SELECT * FROM tblUser WHERE isVerified=0");
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

60ed6be60165c1b8e3b8ba1d44120b22c54a8bf3
zinhlematshiane <ST10447353@rcconnect.edu.za>
2026-06-18 02:09:16 +0200

    updated saveSellerRequest page

--- saveSellerRequest.php
+++ saveSellerRequest.php
@@ -1,13 +1,30 @@
 <?php
 
+include 'DBConn.php';
+
 $brand = $_POST['brand'];
 $description = $_POST['description'];
 $price = $_POST['price'];
 
-echo "<h2>Request Submitted Successfully!</h2>";
+$imageName = $_FILES['image']['name'];
+$tempName = $_FILES['image']['tmp_name'];
 
-echo "Brand: " . $brand . "<br>";
-echo "Description: " . $description . "<br>";
-echo "Price: R" . $price . "<br>";
+move_uploaded_file($tempName, "images/".$imageName);
+
+$sql = "INSERT INTO tblSellerRequest
+(brand, description, price, image, status)
+VALUES
+('$brand', '$description', '$price', '$imageName', 'Pending')";
+
+if($conn->query($sql) === TRUE)
+{
+    echo "Seller request submitted successfully!";
+}
+else
+{
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
 
 ?>

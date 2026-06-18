ad15ebe21d35bfeb6813c63ae18dd48c5277f9a1
zinhlematshiane <ST10447353@rcconnect.edu.za>
2026-06-18 18:37:08 +0200

    updated productDetails page

--- productDetails.php
+++ productDetails.php
@@ -13,32 +13,38 @@ $id = $_GET['id'];
 if ($id == 1) {
     echo "<h1>Vintage Shirt</h1>";
     echo "<img src='images/shirt1.jpg' width='300'>";
+    echo "<p>Description: Stylish second-hand vintage shirt.</p>";
     echo "<p>Price: R250</p>";
     echo "<p>Condition: Good</p>";
 }
 elseif ($id == 2) {
     echo "<h1>Baige Hoodie</h1>";
     echo "<img src='images/hoodie1.jpg' width='300'>";
+    echo "<p>Description: Clean white sneakers with a modern sporty look.</p>";
     echo "<p>Price: R350</p>";
     echo "<p>Condition: Excellent</p>";
 }
 elseif ($id == 3) {
     echo "<h1>White Sneakers</h1>";
     echo "<img src='images/shoes1.jpg' width='300'>";
+    echo "<p>Description: Clean white sneakers with a modern sporty look.</p>";
     echo "<p>Price: R500</p>";
     echo "<p>Condition: Like New</p>";
 }
 elseif ($id == 4) {
     echo "<h1>Leather jacket</h1>";
     echo "<img src='images/jacket1.jpg' width='300'>";
+    echo "<p>Description: Burgundy leather jacket suitable for all seasons.</p>";
     echo "<p>Price: R450</p>";
     echo "<p>Condition: Excellent</p>";
 }
 elseif ($id == 5) {
     echo "<h1>Jeans</h1>";
     echo "<img src='images/jeans1.jpg' width='300'>";
+    echo "<p>Description: Stylish denim jeans in great condition</p>";
     echo "<p>Price: R300</p>";
     echo "<p>Condition: Excellent</p>";
+
 }
 ?>
 
